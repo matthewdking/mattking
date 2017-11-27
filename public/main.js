6a7f22a7a225ec5d9796570c299fe6f36b138dcb
matthewdking <matthew.d.king@outlook.com>
2017-11-27 20:46:24 +0200

    creates typing link functionality #9 :hankey:

--- public/main.js
+++ public/main.js
@@ -1,13 +1,30 @@
 var typedDiv = document.getElementById('typed');
 
 var myDescription =
-  "I'm a full stack javascript developer. I'm a graduate of Founders and Coders.  I am passionate about the environment and sustainability, and always looking for projects with a positive footprint.";
+  "I'm a full stack javascript developer. I'm a graduate of  <a href='https://foundersandcoders.com/' id='faclink'></a>.";
 
-function typeText(element, str) {
+var part2 =
+  ' I am passionate about the environment and sustainability, and always looking for projects with a positive footprint.';
+
+var counter = 0;
+
+function typeText(str, element) {
   setTimeout(() => {
-    element.textContent += str.charAt(0);
-    typeText(element, str.slice(1));
-  }, Math.random() * 200);
+    if (str.charAt(0) === '<') {
+      element.innerHTML += str.substr(0, str.indexOf('>') + 1);
+      var faclink = document.getElementById('faclink');
+      typeText('Founders and Coders.', faclink);
+    } else if (str.length === 0 && counter === 0) {
+      counter++;
+      typeText(part2, typedDiv);
+    } else {
+      element.innerHTML += str.charAt(0);
+      if (str.length === 0) {
+        return;
+      }
+      typeText(str.slice(1), element);
+    }
+  }, Math.random() * 20);
 }
 
-typeText(typedDiv, myDescription);
+typeText(myDescription, typedDiv);
